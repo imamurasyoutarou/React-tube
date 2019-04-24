0f88a5a95597782a12cd6b8ba4c0449128e43d95
imamurasyoutarou <46349073+imamurasyoutarou@users.noreply.github.com>
2019-04-24 11:40:28 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+import YSearch from 'youtube-api-search';
+
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+import List from './components/Video/List/List';
+import Video from './components/Video/Video';
+
+
+const YOUTUBE_API_KEY = 'AIzaSyC4Zaowu05ZczH-jEwAhIs3iU1JrgKRNk8'
+
+class App extends Component {
+
+  state = {
+    videos: [],
+    selectedVideo: null
+  }
+
+  componentDidMount() {
+    YSearch({ key: YOUTUBE_API_KEY, term: 'React プログラミング' }, (data) => {
+      this.setState({ videos: data, selectedVideo: data[2] })
+    });
+  }
+
+  onVideoClickedHandler = (video) => {
+    this.setState({ selectedVideo: video })
+  }
+
+  onKeywordChangedHandler = (keyword) => {
+    let newTerm = 'React プログラミング' + keyword;
+    if (keyword === '') {
+      newTerm = 'React プログラミング';
+    }
+
+    YSearch({ key: YOUTUBE_API_KEY, term: newTerm }, (data) => {
+      this.setState({ videos: data, selectedVideo: data[0] })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header onKeywordChanged={this.onKeywordChangedHandler} />
+        <Body>
+          <Video video={this.state.selectedVideo} />
+          <List
+            videos={this.state.videos}
+            onVideoClicked={this.onVideoClickedHandler}
+            selectedVideo={this.state.selectedVideo}
+          />
+        </Body>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+
+  state = {keyword: ''};
+
+
+  onInputChangeHandler = (event) => {
+    this.setState({keyword: event.target.value});
+    this.props.onKeywordChanged(event.target.value);
+  }
+
+  render(){
+
+    const fontStyle = {
+      fontFamily: 'Special Elite'
+    }
+    const fontColor = {
+      color:'#46EEFF'
+    }
+
+    return(
+      <nav className="navbar navbar-expand
+                    navbar-light bg-light
+                    mb-3 p-3">
+        <h2 className="mb-0" >
+          <span className="d-none d-md-inline"
+            style={fontStyle}
+            style={fontColor}
+
+            >React Tube</span>
+        </h2>
+        <form className="form-group my-2 my-lg-0 ml-sm-2 ">
+          <input
+            onChange={this.onInputChangeHandler} value={this.state.keyword}
+            className="form-control form-control-lg mr-sm-2" type="text" placeholder="検索..." aria-label="検索..." />
+        </form>
+      </nav>
+    );
+  }
+}
+
+export default Header;
